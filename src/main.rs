7df1ce97237ceb964f794013edc2a97d8f0de488
Drew Marshall <thedrewguy@gmail.como>
2024-07-20 16:08:20 -0700

    start strigifier art algo implementation

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use board::Board;
 mod artgenerator;
 mod dither;
 mod kmeans;
+mod stringifier;
 mod util;
 
 fn main() {
---
+++ src/stringifier.rs
@@ -0,0 +1,43 @@
+use image::{DynamicImage, Rgb};
+use std::collections::HashMap;
+
+use crate::{
+    board::{Board, Nail},
+    dither,
+    util::{ArtAlgo, ColorPalette},
+};
+
+struct ColorState {
+    nail: Option<Nail>,
+    mask: Vec<Vec<bool>>,
+}
+
+pub struct Stringifier {
+    color_states: HashMap<Rgb<u8>, ColorState>,
+    remaining_paths: HashMap<(Nail, Nail), Vec<(u32, u32)>>,
+}
+
+impl Stringifier {
+    pub fn new(board: &Board, src_img: &DynamicImage, color_palette: ColorPalette) -> Self {
+        let scaled_img = board.scale_image(src_img, None);
+        let dithered_img = dither::dither_image(&scaled_img, color_palette);
+        let color_masks = dither::get_color_masks(&dithered_img, color_palette);
+
+        let color_states = color_palette.iter().fold(HashMap::new(), |mut acc, color| {
+            let mask = color_masks.get(color).unwrap();
+            acc.insert(
+                *color,
+                ColorState {
+                    nail: None,
+                    mask: mask.clone(),
+                },
+            );
+            acc
+        });
+
+        Self {
+            color_states,
+            remaining_paths: board.getPaths().clone(),
+        }
+    }
+}
